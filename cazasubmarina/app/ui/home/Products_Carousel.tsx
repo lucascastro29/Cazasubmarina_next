2412fc3878307b5446cec2c85b4bdb658104b5cf
lucascastro29 <88244464+lucascastro29@users.noreply.github.com>
2024-04-04 11:02:11 -0300

    carousel items
    the omit before

--- cazasubmarina/app/ui/home/Products_Carousel.tsx
+++ cazasubmarina/app/ui/home/Products_Carousel.tsx
@@ -79,7 +79,7 @@ export default function Products_Carousel(){
                                         <h3 className="text-2xl font-bold text-[#001220]">Maintenance</h3>
                                         <p className="mt-2 text-gray-500 dark:text-gray-400 ml-4 mr-4">We provide ongoing maintenance and support.</p>
 
-                                                
+                                                 
                                     </div>
                                 </div>
                                     </div>
