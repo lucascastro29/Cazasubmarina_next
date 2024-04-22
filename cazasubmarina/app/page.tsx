86a7e6f575f50f06b5f4ca55d9756fc2bceb704f
lucascastro29 <88244464+lucascastro29@users.noreply.github.com>
2024-04-22 08:37:54 -0300

    courses display

--- cazasubmarina/app/page.tsx
+++ cazasubmarina/app/page.tsx
@@ -1,11 +1,14 @@
 import Carousel from "@/app/ui/home/Carousel";
 import Carousel_Products from "./ui/home/Carousel_Products";
+import Courses_display from "@/app/ui/home/Courses_display"
 
 export default function Page() {
   return (
   <>
   <Carousel/>
   <Carousel_Products/>
+
+  <Courses_display/>
   </>
 
   );
