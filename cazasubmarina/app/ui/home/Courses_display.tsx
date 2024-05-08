3ba0e3d0f90d01533fe73ca052381825d9e7027b
lucascastro29 <88244464+lucascastro29@users.noreply.github.com>
2024-05-08 15:33:50 -0300

    cursos displayer
    cambiar imagenes y texto

--- cazasubmarina/app/ui/home/Courses_display.tsx
+++ cazasubmarina/app/ui/home/Courses_display.tsx
@@ -1,6 +1,8 @@
 import bg from "@/public/img/backgroud_courses.jpg"
 
 import { Kanit } from "next/font/google";
+import aletas_1 from "@/public/img/Products/products_img/Aletas/Aletas_seasub_dive_2.jpg"
+import Image from "next/image";
 
 const kanit=Kanit({
   weight:'400',
@@ -21,13 +23,16 @@ export default function courses(){
         <div className="flex items-center justify-center w-full space-x-16 " >
         <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
-        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
     </a>
     <div className="p-5">
-        <a href="#">
+        
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">APNEA</h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        
+            <Image src={aletas_1} alt="design" style={{width:"100%",height:"100%"}} height={1000} width={1000} className="w-24 h-24 mt-4  mb-3 rounded" />
+
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+  
+            Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
         <a href="/Cursos" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Mas Información
              <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -37,13 +42,13 @@ export default function courses(){
     </div>
 </div>
 <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-    </a>
+    
     <div className="p-5">
-        <a href="#">
+        
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CAZASUBMARINA</h5>
-        </a>
+        
+        <Image src={aletas_1} alt="design" style={{width:"100%",height:"100%"}} height={1000} width={1000} className="w-24 h-24 mt-4  mb-3 rounded" />
+
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
         <a href="/Cursos" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Mas Información
